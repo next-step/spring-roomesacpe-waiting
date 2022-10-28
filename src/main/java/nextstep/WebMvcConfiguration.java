60c1bd8bcede1720eb75f8adff5b163522910e14
hyeon9mak <jinha3507@gmail.com>
2022-10-28 21:57:10 +0900

    refactor: auth 패키지 분리

--- src/main/java/nextstep/WebMvcConfiguration.java
+++ src/main/java/nextstep/WebMvcConfiguration.java
@@ -1,32 +1,42 @@
 package nextstep;
 
-import nextstep.auth.AdminInterceptor;
-import nextstep.auth.JwtTokenProvider;
-import nextstep.auth.LoginMemberArgumentResolver;
-import nextstep.auth.LoginService;
+import auth.*;
+import java.util.List;
+import nextstep.member.MemberDao;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
-import java.util.List;
-
 @Configuration
 public class WebMvcConfiguration implements WebMvcConfigurer {
-    private LoginService loginService;
-    private JwtTokenProvider jwtTokenProvider;
 
-    public WebMvcConfiguration(LoginService loginService, JwtTokenProvider jwtTokenProvider) {
-        this.loginService = loginService;
-        this.jwtTokenProvider = jwtTokenProvider;
+    private final MemberDao memberDao;
+
+    public WebMvcConfiguration(MemberDao memberDao) {
+        this.memberDao = memberDao;
     }
 
-    @Override
+    @Bean
+    public JwtTokenProvider jwtTokenProvider() {
+        return new JwtTokenProvider();
+    }
+
+    @Bean
+    public LoginService loginService() {
+        return new LoginService(memberDao, jwtTokenProvider());
+    }
+
+    @Bean
+    public LoginController loginController() {
+        return new LoginController(loginService());
+    }
+
     public void addInterceptors(InterceptorRegistry registry) {
-        registry.addInterceptor(new AdminInterceptor(jwtTokenProvider)).addPathPatterns("/admin/**");
+        registry.addInterceptor(new AdminInterceptor(jwtTokenProvider())).addPathPatterns("/admin/**");
     }
 
-    @Override
     public void addArgumentResolvers(List argumentResolvers) {
-        argumentResolvers.add(new LoginMemberArgumentResolver(loginService));
+        argumentResolvers.add(new LoginMemberArgumentResolver(loginService()));
     }
 }
