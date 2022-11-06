8ce520401819378f9fa65d5144589d843e7fe22f
seunghyang <seunghyang@gmarket.com>
2022-11-06 20:15:24 +0900

    [step2] reservation_waiting 스키마 추가

--- src/main/resources/schema.sql
+++ src/main/resources/schema.sql
@@ -34,3 +34,13 @@ CREATE TABLE member
     role     varchar(20) not null,
     primary key (id)
 );
+
+CREATE TABLE reservation_waiting
+(
+    id          bigint not null auto_increment,
+    schedule_id bigint not null,
+    member_id   bigint not null,
+    canceled    boolean not null,
+    created_at  timestamp not null default current_timestamp,
+    primary key (id)
+);
