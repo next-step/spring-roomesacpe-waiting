91c7ecb9fc2906856099d1e216d187baaa56dfc1
Daeun Lee <leedaeun.dev@gmail.com>
2022-11-07 00:13:24 +0900

    refactor: Component Scan -> Java Configuration으로 Bean 등록

--- src/main/java/auth/JwtTokenProvider.java
+++ src/main/java/auth/JwtTokenProvider.java
@@ -7,9 +7,7 @@ import io.jsonwebtoken.Jwts;
 import io.jsonwebtoken.SignatureAlgorithm;
 import java.util.Date;
 import org.springframework.beans.factory.annotation.Value;
-import org.springframework.stereotype.Component;
 
-@Component
 public class JwtTokenProvider {
 
     @Value("${security.jwt.token.secret-key}")
---
+++ src/test/java/nextstep/auth/AuthE2ETest.java
@@ -0,0 +1,53 @@
+package nextstep.auth;
+
+import auth.TokenRequest;
+import auth.TokenResponse;
+import io.restassured.RestAssured;
+import nextstep.member.MemberRequest;
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.test.annotation.DirtiesContext;
+
+@DirtiesContext(classMode = DirtiesContext.ClassMode.BEFORE_EACH_TEST_METHOD)
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.DEFINED_PORT)
+class AuthE2ETest {
+
+    public static final String USERNAME = "username";
+    public static final String PASSWORD = "password";
+
+    private Long memberId;
+
+    @BeforeEach
+    void setUp() {
+        MemberRequest body = new MemberRequest(USERNAME, PASSWORD, "name", "010-1234-5678",
+            "ADMIN");
+        RestAssured
+            .given().log().all()
+            .contentType(MediaType.APPLICATION_JSON_VALUE)
+            .body(body)
+            .when().post("/members")
+            .then().log().all()
+            .statusCode(HttpStatus.CREATED.value());
+    }
+
+    @DisplayName("토큰을 생성한다")
+    @Test
+    void create() {
+        TokenRequest body = new TokenRequest(USERNAME, PASSWORD);
+        var response = RestAssured
+            .given().log().all()
+            .contentType(MediaType.APPLICATION_JSON_VALUE)
+            .body(body)
+            .when().post("/login/token")
+            .then().log().all()
+            .statusCode(HttpStatus.OK.value())
+            .extract();
+
+        Assertions.assertThat(response.as(TokenResponse.class)).isNotNull();
+    }
+}
